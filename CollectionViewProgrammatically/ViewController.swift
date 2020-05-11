788803bb264415d75d992fd43aeeb4606d323820
jerryngton <jerryngtond@gmail.com>
2020-05-11 09:50:40 +0300

    Initial Commit

---
+++ CollectionViewProgrammatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewProgrammatically
+//
+//  Created by Evgeny on 11.05.2020.
+//  Copyright © 2020 Jerryngton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
